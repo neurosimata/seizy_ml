19897ea76f139b22ea77981fded4a260c89ff4a9
Pantelis Antonoudiou <pantelis.antonoudiou@gmail.com>
2023-08-03 14:40:41 -0400

    Update get_predictions.py

--- data_preparation/get_predictions.py
+++ data_preparation/get_predictions.py
@@ -6,47 +6,55 @@ from tqdm import tqdm
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
-import joblib
+from joblib import load
 # User Defined
 from helper.array_helper import find_szr_idx, merge_close
 from helper import features
 from helper.io_getfeatures import get_data, get_features_allch
 ### ------------------------------------------------------------------------###
-              
+
+# define model and features
+model_name = 'gnb_model'
+feature_select = np.array([1,1,1,1,0,0,0,0,0,0,1,1,1,0,0,0,0,0,1,0,0])
+param_list = (features.autocorr, features.line_length, features.rms, 
+              features.mad, features.var, features.std, features.psd, 
+              features.energy, features.get_envelope_max_diff,)
+cross_ch_param_list = (features.cross_corr, features.signal_covar,
+                       features.signal_abs_covar,)
+
+
 class ModelPredict:
     """
     Class for batch seizure prediction.
     """
     
-    def __init__(self, property_dict):
+    def __init__(self, load_path, save_path, win, fs):
         """
 
         Parameters
         ----------
-        property_dict : Dict, contains essential info for data load and processing
+        load_path : str
+        save_path : str
+        win : int
+        fs : int/float
+
+        Returns
+        -------
+        None.
 
         """
-        
+            
         # get main path and load properties file
-        self.gen_path = property_dict['main_path']
-        self.filt_dir = property_dict['filt_dir']
-        
-        # create raw pred path
-        self.rawpred_dir = os.path.join(self.gen_path, property_dict['rawpred_dir'])
-        
-        # get sampling rate
-        self.fs = property_dict['new_fs']
-        self.win = property_dict['win']
+        self.load_path = load_path
+        self.save_path = save_path
+        self.win = win
+        self.fs = fs    
+        self.erode = 1 # in window bins
+        self.merge = 5 # in window bins
 
-        # read method parameters into dataframe
-        df = pd.read_csv(os.path.join('train','selected_method.csv'))
-        self.thresh = np.array(df.loc[0][df.columns.str.contains('Thresh')])
-        self.weights = np.array(df.loc[0][df.columns.str.contains('Weight')])
-        self.enabled = np.array(df.loc[0][df.columns.str.contains('Enabled')])
-        
-        # get feature names
-        self.feature_names = df.columns[df.columns.str.contains('Enabled')]
-        self.feature_names = np.array([x.replace('Enabled_', '') for x in  self.feature_names])
+        # load model
+        self.feature_select = feature_select
+        self.model = load(model_name +'.joblib')
 
 
     def predict(self):
@@ -55,14 +63,14 @@ class ModelPredict:
         """
        
         print('---------------------------------------------------------------------------\n')
-        print('---> Initiating Predictions for:', self.rawpred_dir + '.', '\n')
+        print('---> Initiating Predictions.', '\n')
        
         # Create path prediction path
-        if os.path.exists(self.rawpred_dir) is False:
-            os.mkdir(self.rawpred_dir)
+        if os.path.exists(self.save_path) is False:
+            os.mkdir(self.save_path)
         
         # Get file list
-        filelist = list(filter(lambda k: '.h5' in k, os.listdir(os.path.join(self.gen_path, self.filt_dir))))
+        filelist = list(filter(lambda k: '.h5' in k, os.listdir(self.load_path)))
         
         # loop files (multilple channels per file)
         for i in tqdm(range(len(filelist)), desc = 'Progress'):
@@ -71,9 +79,9 @@ class ModelPredict:
             data, bounds_pred = self.get_feature_pred(filelist[i].replace('.h5',''))
             
             # Convert prediction to binary vector and save as .csv
-            ModelPredict.save_idx(os.path.join(self.rawpred_dir, filelist[i].replace('.h5','.csv')), data, bounds_pred)
+            ModelPredict.save_idx(os.path.join(self.save_path, filelist[i].replace('.h5','.csv')), data, bounds_pred)
             
-        print('---> Predictions have been generated for: ', self.rawpred_dir + '.','\n')
+        print('---> Predictions have been generated for: ', self.save_path + '.','\n')
         print('---------------------------------------------------------------------------\n')
             
                
@@ -92,33 +100,20 @@ class ModelPredict:
 
         """
         
-        # define parameter list
-        param_list = (features.autocorr, features.line_length, features.rms, 
-                      features.mad, features.var, features.std, features.psd, 
-                      features.energy, features.get_envelope_max_diff,)
-        cross_ch_param_list = (features.cross_corr, features.signal_covar,
-                               features.signal_abs_covar,)
-        
         # get data and true labels
-        data = get_data(self.gen_path, file_id,
-                        inner_path={'data_path':self.filt_dir},
-                        load_y=False)
+        data = get_data(self.gen_path, file_id, inner_path={'data_path':self.filt_dir}, load_y=False)
         
         # Eextract features and normalize
         x_data, labels = get_features_allch(data, param_list, cross_ch_param_list)
         x_data = StandardScaler().fit_transform(x_data)
         
         # get predictions
-        thresh = (np.mean(x_data) + self.thresh * np.std(x_data))               # get threshold vector
-        y_pred_array = (x_data > thresh)                                        # get predictions for all conditions
-        y_pred = y_pred_array * self.weights * self.enabled                     # get predictions based on weights and selected features
-        y_pred = np.sum(y_pred, axis=1) / np.sum(self.weights * self.enabled)   # normalize to weights and selected features
-        y_pred = y_pred > 0.5                                                   # get popular vote
-        bounds_pred = find_szr_idx(y_pred, dur=1)                               # get predicted seizure index
+        y_pred = self.model.predict(x_data[:,self.feature_select])
+        bounds_pred = find_szr_idx(y_pred, dur=self.erode)
         
         # if seizures are detected, merge close segments
         if bounds_pred.shape[0] > 0:
-            bounds_pred = merge_close(bounds_pred, merge_margin=5)
+            bounds_pred = merge_close(bounds_pred, merge_margin=self.merge)
             
         return data, bounds_pred 
 
