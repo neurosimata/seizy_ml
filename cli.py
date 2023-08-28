7ba94b346936772be2c54423d5a4d8cea835dc19
Pantelis Antonoudiou <pantelis.antonoudiou@gmail.com>
2023-08-28 08:16:22 -0400

    Update cli.py

--- cli.py
+++ cli.py
@@ -6,7 +6,6 @@ import os
 import yaml
 ### ----------------------------------------------------------------- ###
 
-
 class CustomOrderGroup(click.Group):
     def __init__(self, **attrs):
         super(CustomOrderGroup, self).__init__(**attrs)
@@ -22,7 +21,6 @@ class CustomOrderGroup(click.Group):
     def list_commands(self, ctx):
         return self.commands_in_order
 
-    
 @click.group(cls=CustomOrderGroup)
 @click.pass_context
 def main(ctx):
@@ -79,6 +77,51 @@ def setpath(ctx, path):
     with open(settings_path, 'w') as file:
         yaml.dump(ctx.obj, file) 
     click.secho(f"\n -> Path was set to:'{path}'.\n", fg='green', bold=True)
+
+@main.command()
+@click.pass_context
+def train(ctx, p):
+    """* Train Model """
+    
+    # check if user input exists in process types
+    process_type_options = ['preprocess', 'get_features', 'parameter_space', 'train']
+    if p is None:
+        process_type = set(process_type_options)
+    else:
+        process_type = set([p])
+    
+    process_type = list(process_type.intersection(process_type_options))
+    if not process_type:
+        click.secho(f"\n -> Got'{p}' instead of {process_type_options}\n",
+                    fg='yellow', bold=True)
+        return
+        
+    train_path = os.path.join(ctx.obj['parent_path'], ctx.obj['train_dir'])
+    if 'preprocess' in process_type:
+        from helper.io import load_data, save_data
+        from helper.preprocess import PreProcess
+        # load data and preprocess
+        data = load_data(os.path.join(train_path, 'x_pre.h5'))
+        obj = PreProcess("", "", fs=ctx.obj['fs'],)
+        x = obj.filter_clean(data)
+        save_data(os.path.join(train_path, 'x.h5'), x)
+
+    if 'get_features' in process_type:
+        from helper.get_features import compute_features
+        features_array, feature_labels = compute_features(data, ctx.obj['single_channel_features'], ctx.obj['cross_channel_features'],
+                         ctx.obj['channels'])
+        
+    if 'parameter_space' in process_type:
+        from get_train_parameters import feature_selection_and_ranking, feature_space
+        # get feature metrics
+        y_true = load_data(os.path.join(train_path, 'x_pre.h5'))
+        feature_metrics = feature_selection_and_ranking(x, y_true, feature_labels)
+        feature_space, ranks = feature_space(feature_metrics, feature_size=[4,8],
+                          export_name='feature_space.csv', export=True)
+        
+    if 'train' in process_type:
+        print('train')
+
         
 @main.command()
 @click.pass_context
@@ -206,8 +249,8 @@ def extractproperties(ctx):
     from helper.get_seizure_properties import get_seizure_prop
     _, save_path = get_seizure_prop(ctx.obj['parent_path'], ctx.obj['verified_predictions_dir'], ctx.obj['win'])
     click.secho(f"\n -> Properies were saved in '{save_path}'.\n", fg='green', bold=True)
-
-
+    
+    
 if __name__ == '__main__':
     
     # define settings path
