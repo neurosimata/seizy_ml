c4b7accc39decc87447f8c2534cde3559b7b0389
pantelis <pantelis71@hotmail.com>
2023-09-11 13:54:56 -0400

    rename and fix bugs
    fix feature selection bug and remove exports from functions

--- train/select_features.py
+++ train/select_features.py
@@ -7,7 +7,7 @@ from sklearn.feature_selection import mutual_info_classif
 ##### ------------------------------------------------------------------- #####
 
 
-def feature_selection_and_ranking(x, y_true, feature_labels, r_threshold=0.99):
+def feature_selection_and_ranking(x, y_true, feature_labels):
     """
     This function performs feature selection by eliminating highly correlated features and then ranks 
     the selected features using ANOVA F-value and mutual information.
@@ -15,14 +15,14 @@ def feature_selection_and_ranking(x, y_true, feature_labels, r_threshold=0.99):
     Parameters:
     x (numpy array): Array containing feature values
     y_true (numpy array): Array containing true labels
-    feature_labels (list): List of feature labels.
-    r_threshold (float): Remove highly correlated features above the value.
+    feature_labels (list): List of feature labels
     
     Returns:
     None. A csv file named 'feature_metrics.csv' is saved in the current directory.
     """
 
     # Select features that are not highly correlated before proceeding with ranking
+    r_threshold = 0.99
     corr_matrix = np.corrcoef(x.T)
     corr_matrix = pd.DataFrame(corr_matrix, index=feature_labels, columns=feature_labels)
     corr_matrix = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(bool))
@@ -37,28 +37,29 @@ def feature_selection_and_ranking(x, y_true, feature_labels, r_threshold=0.99):
     selected_high_corr_features = set(corr_feature_df.iloc[:,0].values)
     drop_features = high_corr_features.difference(selected_high_corr_features)
     selected_features = set(corr_matrix.index).difference(drop_features)
-    list(corr_matrix.index)
     
+    # put them back in feature original order
     selected_feature_idx = np.zeros(len(feature_labels), dtype=bool)
     for feature in selected_features:
         selected_feature_idx[corr_matrix.index == feature] = True
+    selected_features = feature_labels[selected_feature_idx]
     
     # Get f-values based on ANOVA
     f_vals_anova, _ = f_classif(x[:,selected_feature_idx], y_true)
-    
+
     # Get MI based on mutual information
     mutual_info = mutual_info_classif(x[:, selected_feature_idx], y_true)
     
     # Map back to full feature space
     anova_ranks = np.empty(len(feature_labels), dtype=np.dtype('U100'))
-    sorted_indices =  (1/f_vals_anova).argsort()
+    sorted_indices = (1/f_vals_anova).argsort()
     ranks = sorted_indices.argsort() + 1
     for feature, rank in zip(selected_features, ranks):
         anova_ranks[corr_matrix.index == feature] = str(rank)
-        
+ 
     # Map back to full feature space
     mutual_info_ranks = np.empty(len(feature_labels), dtype=np.dtype('U100'))
-    sorted_indices =  (1/mutual_info).argsort()
+    sorted_indices = (1/mutual_info).argsort()
     ranks = sorted_indices.argsort() + 1
     for feature, rank in zip(selected_features, ranks):
         mutual_info_ranks[corr_matrix.index == feature] = str(rank)
@@ -68,21 +69,18 @@ def feature_selection_and_ranking(x, y_true, feature_labels, r_threshold=0.99):
                                 ['anova_ranks'] + list(anova_ranks),
                                 ['mutual_info_ranks'] + list(mutual_info_ranks)],
                                 columns=['metrics'] + list(feature_labels))
-    
-    feature_metrics.to_csv('feature_metrics.csv', index=False)
+    return feature_metrics
 
 
-def feature_space(feature_metrics, feature_size=[4,8],
-                  export_name='feature_space.csv', export=True):
+def get_feature_space(feature_ranks, feature_size=[33,66],
+                  export_name='feature_space.csv'):
     """
     Create feature space from anova and mutual info ranks dataframe.
 
     Parameters
     ----------
-    feature_metrics : pandas df, containing mnetrics
-    feature_size : list, The default is [4,8].
-    export_name : str, The default is 'feature_space.csv'.
-    export : bool, The default is True.
+    feature_ranks : pandas df, containing mnetrics
+    feature_size : list, Percent of features to select from ANOVA and MI ranks.
 
     Returns
     -------
@@ -92,10 +90,10 @@ def feature_space(feature_metrics, feature_size=[4,8],
     """
 
     # reformat dataframe to get features
-    dft = dft = feature_metrics.drop('metrics', axis=1)
+    dft = dft = feature_ranks.drop('metrics', axis=1)
     dft = dft.T.reset_index()
-    dft.columns = ['features'] + list(feature_metrics['metrics'])
-    df = feature_metrics.drop('metrics', axis=1)
+    dft.columns = ['features'] + list(feature_ranks['metrics'])
+    df = feature_ranks.drop('metrics', axis=1)
     df = df.apply(pd.to_numeric)
     
     # get rank columns and set empty ranks(discarded because of high correlation) to max length
@@ -111,28 +109,18 @@ def feature_space(feature_metrics, feature_size=[4,8],
     bool_array[0,:] = dft[rank_cols[0]]<=max_rank
     cntr = 1
     for rank_col in rank_cols:
-        for feature_len in feature_size:
+        for feature_percent in feature_size:
+            feature_len = int(feature_percent*len(feature_ranks.columns))
             idx = dft.nsmallest(feature_len, columns=rank_col).index.values
             bool_array[cntr, idx] = 1
             cntr+=1
             
     # merge dfs
     feature_space = pd.DataFrame(columns=df.columns, data=bool_array)
-    if export:
-        feature_space.to_csv(export_name, index=False)
-    return feature_space, ranks
+    return feature_space
 
 
-if __name__ == '__main__':
-    
-    data = np.random.rand(51,500,3)
-    
-    # get feature metrics
-    feature_metrics = feature_selection_and_ranking(x, y_true, feature_labels)
-    
-    # create feature space df for training
-    feature_space, ranks = feature_space(feature_metrics, feature_size=[4,8],
-                      export_name='feature_space.csv', export=True)
+
     
     
 
