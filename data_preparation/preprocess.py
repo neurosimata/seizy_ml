59b24acb9fe24f6494f4115aed3706ed9f822d3d
Pantelis Antonoudiou <pantelis.antonoudiou@gmail.com>
2023-09-20 09:03:07 -0400

    add docstring

--- data_preparation/preprocess.py
+++ data_preparation/preprocess.py
@@ -13,28 +13,52 @@ from scipy.signal import butter, zpk2sos, sosfiltfilt
 
 class PreProcess:
     """
-    Remove outliers and filer data from parent folder.
+    A class for preprocessing data by removing outliers and filtering data.
+
+    Attributes
+    ----------
+    load_path : str
+        The path to the directory containing the data to be preprocessed.
+    save_path : str
+        The path to the directory where the preprocessed data will be saved.
+    fs : int
+        The sampling frequency of the data.
+    outier_threshold : int/float, optional
+        The threshold for removing outliers. Default is 25.
+    freq_cutoff : list, optional
+        A list containing a single numeral for the high-pass filter cutoff frequency. Default is [2].
+
+    Methods
+    -------
+    filter_data()
+        Preprocesses the data in the parent folder.
+    filter_clean(data)
+        Filters and removes outliers from the data.
+
     """
-    
-    def __init__(self, load_path, save_path, fs,
-                 outier_threshold=25, freq_cutoff=[2]):
+
+    def __init__(self, load_path, save_path, fs, outier_threshold=25, freq_cutoff=[2]):
         """
-        
+        Initializes the PreProcess class.
 
         Parameters
         ----------
         load_path : str
+            The path to the directory containing the data to be preprocessed.
         save_path : str
+            The path to the directory where the preprocessed data will be saved.
         fs : int
-        outier_threshold : int/float
-        freq_cutoff : list, containing a numeral for hpfilter cutoff.
+            The sampling frequency of the data.
+        outier_threshold : int/float, optional
+            The threshold for removing outliers. Default is 25.
+        freq_cutoff : list, optional
+            A list containing a single numeral for the high-pass filter cutoff frequency. Default is [2].
 
         Returns
         -------
         None.
 
         """
-
         self.load_path = load_path
         self.save_path = save_path
         self.fs = fs
@@ -43,52 +67,56 @@ class PreProcess:
 
     def filter_data(self):
         """
-        Preprocess data in parent folder.
-    
+        Preprocesses the data in the parent folder.
+
+        Returns
+        -------
+        None.
+
         """
-        
         # create save path if it doesn't exist
         if os.path.isdir(self.save_path) == 0:
             os.mkdir(self.save_path)
-        
-        # get file list 
+
+        # get file list
         filelist = list(filter(lambda k: '.h5' in k, os.listdir(self.load_path)))
-        
+
         print('\n --->', len(filelist), 'files will be processed.\n')
-        for i in tqdm(range(0, len(filelist)), desc='Progress:'): # loop through experiments 
-        
+        for i in tqdm(range(0, len(filelist)), desc='Progress:'): # loop through experiments
+
             # clean and filter data
             data = load_data(os.path.join(self.load_path, filelist[i]))
             data = self.filter_clean(data)
-            
+
             # save clean data
             save_data(self.save_path, filelist[i], data)
-            
+
         print('Files in', self.load_path, 'directory have been cleaned and saved in:',
               '-', self.save_path, '-')
         print('---------------------------------------------------------------------------\n')
-            
-    
+
     def filter_clean(self, data):
         """
-        Filter and remove outliers.
-         
+        Filters and removes outliers from the data.
+
+        Parameters
+        ----------
         data : 3d ndarray, (1d = segments, 2d = time, 3d = channels)
-        clean : bool, if true remove outliers
-        filt : bool, if true filter data
-    
+            The data to be filtered and cleaned.
+
         Returns
         -------
         data : 3d ndarray, (1d = segments, 2d = time, 3d = channels)
+            The filtered and cleaned data.
+
         """
         dim = data.shape # get data dimensions
-        
-    
+
         if self.outier_threshold is not None: # if true remove outliers
             for i in range(data.shape[2]):
                 temp = clean_signal(data[:,:,i].flatten(), threshold=self.outier_threshold)
                 data[:,:,i] = temp.reshape((dim[0], dim[1]))
-    
+
         if self.freq_cutoff is not None:
             for i in range(data.shape[2]):
                 # high-pass filter data
