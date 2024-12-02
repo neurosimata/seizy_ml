525d0c4bcae7b2dd51ce53345ad622e9381460c2
Pantelis Antonoudiou <pantelis.antonoudiou@gmail.com>
2024-12-02 16:24:17 -0500

    new features, and modify inputs
    - remove numba
    - add kwargs to features with only signal input

--- helper/features.py
+++ helper/features.py
@@ -2,20 +2,51 @@
 
 ### -------------- IMPORTS -------------- ###
 import numpy as np
-from numba import jit
 from scipy.fftpack import fft
+import scipy.stats as stats
 ### ------------------------------------- ###
 
 
-def psd(signal, fs=256, freq=[2,40]):
+# =============================================================================
+#  Single Features
+# =============================================================================
+
+def get_psd(signal, fs):
     """
-    Measure the power of the signal over the signal over 2-40 Hz.
+    Calculate Power spectral Density.
+
+    Parameters
+    ----------
+    signal : 1D numpy array
+    fs: sampling rate
+
+    Returns
+    -------
+    freqs : 1D numpy array
+    psd : 1D numpy array
+
+
+    """
+    # get power spectrum and normalize to signal length
+    xdft = np.square(np.absolute(fft(signal)))
+    xdft = xdft * (1/(fs*signal.shape[0]))
+       
+    # multiply *2 to conserve energy in positive frequencies
+    psd = 2*xdft[0:int(xdft.shape[0]/2+1)]
+    
+    # generate corresponding frequency bins
+    freqs = np.linspace(0, fs / 2, len(psd))
+    return  freqs, psd
+
+def get_power_area(signal, freq, fs):
+    """
+    Measure the power area of the signal.
     
     Parameters
     ----------
     signal : 1D numpy array
-    fs : int, sampling rate
     freq: list(low, high)
+    fs: sampling rate
     
     Returns
     -------
@@ -23,37 +54,66 @@ def psd(signal, fs=256, freq=[2,40]):
     
     """
     
-    # get frequency Boundaries
-    f_res = signal.shape[0]/fs
-    flow = int(freq[0]*f_res)
-    fup = int(freq[1]*f_res) + 1
-       
-    # get power spectrum and normalize to signal length
-    xdft = np.square(np.absolute(fft(signal)))
-    xdft = xdft * (1/(fs*signal.shape[0]))
-       
-    # multiply *2 to conserve energy in positive frequencies
-    psdx = 2*xdft[0:int(xdft.shape[0]/2+1)] 
-    return np.sum(psdx[flow:fup])
+    freqs, psd = get_psd(signal, fs)
+    flow = np.argmin(np.abs(freqs - freq[0]))
+    fup = np.argmin(np.abs(freqs - freq[1]))
+    return np.sum(psd[flow:fup+1])
 
-def delta_power(signal):
-    return psd(signal, fs=256, freq=[1, 4])
+def weighted_frequency(signal, fs):
+    """
+    Compute the weighted mean frequency (spectral centroid) of a signal.
 
-def alpha_power(signal):
-    return psd(signal, fs=256, freq=[4.2, 8])
+    Parameters
+    ----------
+    signal : 1D numpy array
+    fs: sampling rate
+    
+    Returns
+    -------
+    wm_frequency : np.float
 
-def theta_power(signal):
-    return psd(signal, fs=256, freq=[8.2, 12])
+    """
+    freqs, psd = get_psd(signal, fs)
+    wm_frequency = np.sum(freqs * psd) / np.sum(psd)
+    return wm_frequency
 
-def beta_power(signal):
-    return psd(signal, fs=256, freq=[12.2, 30])
+def spectral_entropy(signal, fs):
+    """
+    Compute the spectral entropy of a normalized power spectral density (PSD).
 
-def gamma_power(signal):
-    return psd(signal, fs=256, freq=[30.2, 49])
+    Parameters
+    ----------
+    psd : 1D numpy array
+        Normalized power spectral density array.
+    fs: sampling rate
+
+    Returns
+    -------
+    spectral_ent : float
+        Spectral entropy value.
+    """
+    _, psd = get_psd(signal, fs)
+    psd_norm = psd / np.sum(psd)
+    spectral_ent = stats.entropy(psd_norm)
+    return spectral_ent
+
+def delta_power(signal, fs):
+    return get_power_area(signal, fs=fs, freq=[2, 4])
+
+def theta_power(signal, fs):
+    return get_power_area(signal, fs=fs, freq=[4.2, 8])
+
+def alpha_power(signal, fs):
+    return get_power_area(signal, fs=fs, freq=[8.2, 12])
+
+def beta_power(signal, fs):
+    return get_power_area(signal, fs=fs, freq=[12.2, 30])
+
+def gamma_power(signal, fs):
+    return get_power_area(signal, fs=fs, freq=[30.2, 50])
 
 
-@jit(nopython=True)
-def line_length(signal):
+def line_length(signal, **kwargs):
     """
     Measures the line length of a signal.
 
@@ -63,34 +123,42 @@ def line_length(signal):
     -------
     line_length : np.float
     """
-    # initialize line length
-    line_lengths = np.zeros(1)
-    line_lengths = line_lengths.astype(np.double)
-    
-    for i in range(1, signal.shape[0]): # loop though signal
-        line_lengths += np.abs(signal[i] - signal[i-1])
+    return np.sum(np.abs(np.diff(signal))) / len(signal)
 
-    # remove zero-length channels
-    line_lengths = line_lengths[np.nonzero(line_lengths)] 
-    if line_lengths.size == 0: return 0.0
-
-    # take the median and normalize by clip length
-    line_length = np.median(line_lengths) / signal.shape[0]
-    return line_length
-
-@jit(nopython=True)
-def std(signal):
+def skewness(signal, **kwargs):
     """
-    Measures standard deviation of a signal.
+    Compute the skewness of a signal.
 
     Parameters
     ----------
     signal : 1D numpy array
-    """
-    return np.std(signal)
+        Input signal.
 
-@jit(nopython=True)
-def var(signal):
+    Returns
+    -------
+    skewness_value : float
+        Skewness of the signal.
+    """
+    return stats.skew(signal)
+
+def kurtosis(signal, **kwargs):
+    """
+    Compute the kurtosis of a signal.
+
+    Parameters
+    ----------
+    signal : 1D numpy array
+        Input signal.
+
+    Returns
+    -------
+    kurtosis_value : float
+        Kurtosis of the signal.
+    """
+    return stats.kurtosis(signal)
+
+
+def var(signal, **kwargs):
     """
     Measures variance of a signal.
 
@@ -100,8 +168,46 @@ def var(signal):
     """
     return np.var(signal)
 
-@jit(nopython=True)
-def rms(signal):
+
+def hjorth_mobility(signal, **kwargs):
+    """
+    Compute the Hjorth Mobility parameter of a signal.
+
+    Parameters
+    ----------
+    signal : 1D numpy array
+        Input signal.
+
+    Returns
+    -------
+    mobility : float
+        Hjorth Mobility of the signal.
+    """
+    diff_signal = np.diff(signal)
+    return np.sqrt(np.var(diff_signal) / np.var(signal))
+
+
+def hjorth_complexity(signal, **kwargs):
+    """
+    Compute the Hjorth Complexity parameter of a signal.
+
+    Parameters
+    ----------
+    signal : 1D numpy array
+        Input signal.
+
+    Returns
+    -------
+    complexity : float
+        Hjorth Complexity of the signal.
+    """
+    diff_signal = np.diff(signal)
+    diff_diff_signal = np.diff(diff_signal)
+    mobility = np.sqrt(np.var(diff_signal) / np.var(signal))
+    complexity = np.sqrt(np.var(diff_diff_signal) / np.var(diff_signal)) / mobility
+    return complexity
+
+def rms(signal, **kwargs):
     """
     Measures root mean square of a signal.
 
@@ -111,8 +217,7 @@ def rms(signal):
     """
     return np.sqrt(np.mean(np.square(signal)))
 
-@jit(nopython=True)
-def max_envelope(signal, win):
+def max_envelope(signal, win, **kwargs):
     """
     Calculates max envelope of a signal across each window.
 
@@ -127,10 +232,10 @@ def max_envelope(signal, win):
     env = np.zeros(signal.shape[0])
     for i in range(0,signal.shape[0]): 
         env[i] = np.max(signal[i:i+win])
+    env[-win + 1:] = env[-win]
     return env
 
-@jit(nopython=True)
-def get_envelope_max_diff(signal):
+def get_envelope_max_diff(signal, **kwargs):
     """
     Measures the sum of the differences between the upper and lower envelopes.
 
@@ -148,19 +253,17 @@ def get_envelope_max_diff(signal):
     low_env = -max_envelope(-signal, win)  
     return np.sum(up_env - low_env)
     
-@jit(nopython=True)
-def mad(signal):
+def mad(signal, **kwargs):
     """
-    Measures mean absolute deviation of a signal.
+    Measures median absolute deviation of a signal.
 
     Parameters
     ----------
     signal : 1D numpy array
     """
-    return np.mean(np.abs(signal - np.mean(signal)))
+    return np.median(np.abs(signal - np.mean(signal)))
 
-@jit(nopython=True)
-def energy(signal):
+def energy(signal, **kwargs):
     """
     Measures the energy of a signal.
 
@@ -171,5 +274,4 @@ def energy(signal):
     energy : np.float
     """
     return np.sum(np.square(signal))
-
 
